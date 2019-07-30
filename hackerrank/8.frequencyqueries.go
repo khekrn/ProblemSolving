493128230748682654fa93899814fea416602400
K Kishore <kishorenayark@gmail.com>
2019-07-30 14:34:07 +0530

    partially completed this problem

---
+++ hackerrank/8.frequencyqueries.go
@@ -0,0 +1,50 @@
+package hackerrank
+
+// FreqQuery implementation
+// https://www.hackerrank.com/challenges/frequency-queries/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=dictionaries-hashmaps
+func FreqQuery(queries [][]int32) []int32 {
+	dict := map[int32]int32{}
+	var nothing struct{}
+
+	res := make([]int32, 0)
+
+	for _, arr := range queries {
+		op := arr[0]
+		value := arr[1]
+
+		switch op {
+		case 1:
+			if count, exist := dict[value]; exist {
+				dict[value] = count + 1
+			} else {
+				dict[value] = 1
+			}
+			break
+		case 2:
+			if count, exist := dict[value]; exist {
+				if count-1 > 0 {
+					dict[value] = count - 1
+				} else {
+					delete(dict, value)
+				}
+			}
+			break
+		default:
+			found := false
+			for _, val := range dict {
+				if val == value {
+					found = true
+					break
+				}
+			}
+			if found {
+				res = append(res, 1)
+			} else {
+				res = append(res, 0)
+			}
+			break
+		}
+	}
+
+	return res
+}
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	s1 := "hi"
-	s2 := "world"
-	fmt.Println(hackerrank.TwoStrings(s1, s2))
+	q := [][]int32{{1, 1}, {2, 2}, {3, 2}, {1, 1}, {1, 1}, {2, 1}, {3, 2}}
+	res := hackerrank.FreqQuery(q)
+	fmt.Println(res)
 }
