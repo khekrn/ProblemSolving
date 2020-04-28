a156023b8a5bc92090ead94ed0bc759c94cd074f
khekrn <kishorenayark@gmail.com>
2020-04-28 19:01:33 +0530

    Reverse Array Fix

--- array/ReverseArray.java
+++ array/ReverseArray.java
@@ -1,5 +1,6 @@
 package array;
 
+
 /**
  * @author khekrn
  *     <p>Reverse array in place
@@ -10,7 +11,7 @@ public class ReverseArray {
     if (arr.length > 0) {
       var leftIndex = 0;
       var rightIndex = arr.length - 1;
-      while (leftIndex <= rightIndex) {
+      while (leftIndex < rightIndex) {
         var temp = arr[leftIndex];
         arr[leftIndex] = arr[rightIndex];
         arr[rightIndex] = temp;
